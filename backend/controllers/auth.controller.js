e02f116c4ae0081df6e1b123cde058a913796890
Issiah Banda <jarzarr@gmail.com>
2023-07-05 17:02:55 -0600

    Setup testing in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,51 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const { findUserById, findUserByUsername } = require('../models/user.model.js');
+
 function setupStrategy() {
     passport.use(
         new LocalStrategy((username, password, done) => {
-    
+            findUserByUsername(username)
+                .then((user) => {
+                    // Error with finding user
+                    if (user === null) {
+                        return done(null, false, { message: 'Incorrect username.' });
+                    }
+                    
+                    // Incorrect password
+                    if (user.password !== password) {
+                        return done(null, false, { message: 'Incorrect password.' });
+                    }
+
+                    // Authentication successful
+                    return done(null, user);
+                });
         })
     );
 
-    // Using default serialization of whole user object
+    // Serializing only the user id 
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        const user = findUserById(id)
+            .then((user) => {
+                if (user === null) {
+                    return done(new Error('Unable to find user with id: '+id));
+                }
+        
+                return done(null, user);
+            });
+    });
 }
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect('/login');
+    
 }
 
 module.exports = {
--- backend/routes/main.routes.js
+++ backend/routes/main.routes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const { ensureAuthenticated } = require('../controllers/auth.controller.js');
 
-router.post('/login', (req, res) => {
-
-});
+router.post('/login', passport.authenticate('local', { successRedirect: '/home', failureRedirect: '/login'}));
 
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 })
-
-router.get('/profile', ensureAuthenticated, (req, res) => {
-
+ // TODO send whether or not authenticated
+router.get('/auth', ensureAuthenticated, (req, res) => {
+    res.sendStatus(403);
 });
 
 module.exports = router;
